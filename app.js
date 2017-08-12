26bff8fd4b83d6e6b387aac9437bceb5ef83f09e
MidasVE <melkvat@gmail.com>
2017-08-12 09:59:53 +0200

    login view added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const pug = require('pug');
+
+const compiledFunction = pug.compileFile('views/login.pug');
+
+app.get('/', (req, res) => {
+    res.send(compiledFunction({
+    title: 'Login'
+}));
+})
+
+app.listen(3000, () => {
+    console.log('Example listening on port 3000!')
+})
