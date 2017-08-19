00d564178c7ebc506e6dda1341ee9137f8d588a6
MidasVE <melkvat@gmail.com>
2017-08-19 13:55:15 +0200

    added layout and setup stylesheets

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'liveqa'}));
 app.use(passport.initialize());
 app.use(passport.session());
