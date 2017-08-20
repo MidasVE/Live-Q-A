326ae3864cdfc88b92e373ea0b91a906659b6011
MidasVE <melkvat@gmail.com>
2017-08-20 10:32:55 +0200

    hardcoded discussion added

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/home', function (req, res, next) {
     res.render('home', {
-        user : req.user.name
+        user : req.user.name,
+        id: 3
     });
 });
 
